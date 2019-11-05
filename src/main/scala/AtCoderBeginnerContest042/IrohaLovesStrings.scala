21d3d617d7322d4f5f92dd5157310b26be45f172
金正朋也 <mhtnim1023@gmail.com>
2019-11-06 01:00:23 +0900

    implement IrohaLovesStrings

---
+++ src/main/scala/AtCoderBeginnerContest042/IrohaLovesStrings.scala
@@ -0,0 +1,14 @@
+package AtCoderBeginnerContest042
+
+class IrohaLovesStrings {
+  def main(args: Array[String]): Unit = {
+    val input = new java.util.Scanner(System.in)
+    val countOfWords = input.toString.split(" ")(0).toInt
+    def getWordsList(i: Int, list: List[String]): List[String] = {
+      val word = new java.util.Scanner(System.in)
+      if(countOfWords < i ) list else getWordsList(i + 1, list :+ word)
+    }
+    val wordsList: List[String] = getWordsList(1, List.empty)
+    println(wordsList.sorted.mkString(""))
+  }
+}
